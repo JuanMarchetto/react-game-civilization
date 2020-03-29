e233b1f92201e8ea1951e6d89fe12ec88da02b76
Juan Marchetto <juanpatriciomarchetto@gmail.com>
2020-03-29 20:26:57 -0300

    functional components and creating login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,42 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Map from "./components/map";
 import Resourses from "./components/resourses";
 import Constructions from "./components/constructions";
 import axios from "axios";
+import Login from "./components/login";
 const baseURL = "http://localhost:5000/";
 
-class App extends Component {
-  state = {
-    resourses: {
-      gold: 999
-    },
-    buildings: [],
-    buildingOptions: [
-      {
-        type: "house",
-        html: <i class="fas fa-home"></i>
-      }
-    ]
-  };
+const App = () => {
+  const [user, setUser] = useState(null);
 
-  render() {
-    return (
-      <>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-12">
-              <Resourses resourses={this.state.resourses} />
-            </div>
-            <div className="col-12">
-              <Map buildings={this.state.buildings} />
-            </div>
+  return user ? (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <Resourses resourses={this.state.resourses} />
+          </div>
+          <div className="col-12">
+            <Map buildings={this.state.buildings} />
           </div>
         </div>
-        <div
-          className="container-fluid"
-          style={{ position: "absolute", bottom: "0" }}
-        >
-          <Constructions
-            buildingOptions={this.state.buildingOptions}
-            OnAddConstruction={this.HandleAddConstruction}
-          />
-        </div>
-      </>
-    );
-  }
-
-  HandleAddConstruction = construcction => {
-    let newConstrucction = {
-      type: construcction.type,
-      html: construcction.html,
-      level: 1,
-      id: Date.now()
-    };
-    const buildings = [...this.state.buildings, newConstrucction];
-    this.setState({ buildings });
-    axios
-      //  .post(baseURL + "users/add", { username: "Juan" })
-      .get(baseURL + "users")
-      .then(res => console.log(res.data));
-  };
-}
+      </div>
+      <div
+        className="container-fluid"
+        style={{ position: "absolute", bottom: "0" }}
+      >
+        <Constructions
+          buildingOptions={this.state.buildingOptions}
+          OnAddConstruction={this.HandleAddConstruction}
+        />
+      </div>
+    </>
+  ) : (
+    <Login />
+  );
+};
 
 export default App;
+/* .post(baseURL + "users/add", { username: "Juan" })
+.get(baseURL + "users")
+.then(res => console.log(res.data));*/
